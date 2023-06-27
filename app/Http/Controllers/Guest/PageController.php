f1f45d6e238e0fb905f6dc27d816582726cd4f4e
AlessioAbbati <alex_117@live.it>
2023-06-27 19:12:23 +0200

    filter train intime and delete

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -9,7 +9,10 @@ use App\Http\Controllers\Controller;
 class PageController extends Controller
 {
     public function index() {
-        $trains = Train::all();
+        // filtro 
+        $trains = Train::all()->filter(function ($train) {
+            return !$train->on_time || !$train->deleted;
+        });
 
         return view ('index', [
             'trains' => $trains
